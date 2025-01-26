279de303dc71edbe30f7f39b10b2c5aeeb7042b2
Dalton-K-Hughes <dhughes3223@gmail.com>
2025-01-26 13:48:35 -0500

    Fixed issue with banner not properly displaying, added a navbar to the header when the user scrolls down

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const banner = document.getElementById('banner');
+const h1 = document.getElementById('banner-h1')
+const nav_bar = document.getElementById('navig')
+nav_bar.style.display = 'none'
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
         banner.classList.add('shrink');
-    } else if(window.scrollY <= 50) {
-        banner.classList.remove('shrink');
+        h1.innerText = 'Urban Legends'
+        nav_bar.style.display = 'block'
     }
 });
